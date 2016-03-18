20a31e2f2fb144559b32835dcd116320672669a1
Joshua924 <sz1288@nyu.edu>
2016-03-18 15:32:01 -0400

    Almost done ....

--- src/edu/nyu/wse/qx307/ScoredURL.java
+++ src/edu/nyu/wse/qx307/ScoredURL.java
@@ -1,28 +1,41 @@
 package edu.nyu.wse.qx307;
 
 import java.net.URL;
+import java.util.Date;
 
 public class ScoredURL implements Comparable<ScoredURL> {
   private URL url;
   private int score;
+  private Date date;
   
   public ScoredURL(URL url, int score) {
     this.url = url;
     this.score = score;
+    date = new Date(System.currentTimeMillis());
   }
   
   @Override
   public int compareTo(ScoredURL o) {
-    return score - o.score;
+    if (score != o.score) {
+      return score - o.score;
+    }
+    return o.date.compareTo(date);
   }
   
   @Override
   public String toString() {
     return url.toString() + ". Score = " + score;
-    
   }
   
   public URL getURL() {
     return url;
+  }
+  
+  public int getScore() {
+    return score;
+  }
+  
+  public void incrementScore(int score) {
+    this.score += score;
   }
 }
--- src/edu/nyu/wse/qx307/WebCrawler.java
+++ src/edu/nyu/wse/qx307/WebCrawler.java
@@ -1,10 +1,10 @@
 package edu.nyu.wse.qx307;
 
-// A minimal Web Crawler written in Java
+// A extended Web Crawler written in Java
 // Usage: From command line 
-//     java WebCrawler <URL> [N]
-//  where URL is the url to start the crawl, and N (optional)
-//  is the maximum number of pages to download.
+//     java WebCrawler -u <URL> -q <queries> -docs <directory>
+//     -m <MaxNumberOfPages> [-t if enable trace]
+//  where URL is the url to start the crawling.
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -14,6 +14,7 @@ import java.io.PrintWriter;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -26,18 +27,18 @@ import java.util.Properties;
 import java.util.Set;
 
 public class WebCrawler {
-  public static final int SEARCH_LIMIT = 20; // Absolute max pages
-  public static final boolean DEBUG = false;
+  public static final int SEARCH_LIMIT = 20;
   public static final String DISALLOW = "Disallow:";
-  public static final int MAXSIZE = 20000; // Max size of file
+  public static final int MAXSIZE = 20000;
 
   // URLs to be searched
   PriorityQueue<ScoredURL> newURLs;
   Map<URL, Integer> knownURLs;
+  Map<URL, ScoredURL> urlsInQueue;
   URL initialURL;
-  int maxPages;
   Set<String> queries;
   File directory;
+  int maxPages;
   boolean trace;
 
   private void parseArgs(String opt, String val) {
@@ -46,7 +47,10 @@ public class WebCrawler {
     } else if (opt.equals("-u")) {
       try {
         initialURL = new URL(val);
-        newURLs.add(new ScoredURL(initialURL, 0));
+        knownURLs.put(initialURL, 1);
+        ScoredURL e = new ScoredURL(initialURL, 0);
+        newURLs.add(e);
+        urlsInQueue.put(initialURL, e);
       } catch (MalformedURLException e) {
         throw new IllegalArgumentException("URL invalid");
       }
@@ -65,11 +69,12 @@ public class WebCrawler {
   public void initialize(String[] argv) {
     newURLs = new PriorityQueue<>(SEARCH_LIMIT, Collections.reverseOrder());
     knownURLs = new HashMap<URL, Integer>();
+    urlsInQueue = new HashMap<URL, ScoredURL>();
     int i = 0;
-    while(i < argv.length) {
-      if(argv[i].equals("-q")) {
+    while (i < argv.length) {
+      if (argv[i].equals("-q")) {
         String val = "";
-        while(!argv[++i].startsWith("-")) {
+        while (!argv[++i].startsWith("-")) {
           val += argv[i] + " ";
         }
         parseArgs("-q", val.trim());
@@ -81,9 +86,9 @@ public class WebCrawler {
         i += 2;
       }
     }
-    if(trace) {
+    if (trace) {
       System.out.println("Crawling for " + maxPages + " pages relevant to "
-          + queries + " starting from " + initialURL);
+          + queries + " starting from " + initialURL + "\n");
     }
 
     /*
@@ -141,44 +146,43 @@ public class WebCrawler {
 
   private List<String> removeTagItems(List<String> words) {
     List<String> res = new ArrayList<String>();
-    for(String word : words) {
-      if(word.matches("[A-Za-z0-9.]*") && !word.equals("href")) {
+    for (String word : words) {
+      if (word.startsWith(">")) {
+        word = word.substring(1);
+      }
+      if (word.matches("[A-Za-z0-9.]*") && !word.equals("href")) {
         res.add(word);
       }
     }
     return res;
   }
-  
+
   public int score(String newUrlString, String lcPage, String anchor,
       Set<String> queries) {
     if (queries == null || queries.isEmpty()) {
       return 0;
     }
     Set<String> fourPointSet = new HashSet<String>();
-    Set<String> onePointSet = new HashSet<String>();
     int score = 0;
     for (String query : queries) {
-      boolean dontCountAgain = false;
       if (anchor.indexOf(query) >= 0) {
         score += 50;
-        dontCountAgain = true;
+        // add it to the four point set to avoid counting again
+        fourPointSet.add(query);
       }
-      if (newUrlString.indexOf(query) >= 0) {
+      if (newUrlString.toLowerCase().indexOf(query) >= 0) {
         score += 40;
-        dontCountAgain = true;
-      }
-      if (lcPage.indexOf(query) >= 0 && !dontCountAgain) {
-        onePointSet.add(query);
+        fourPointSet.add(query);
       }
     }
 
-    // remove html tags and tokenize the content
-    List<String> words = Arrays.asList(lcPage.replaceAll("[/=\",]", " ")
+    // remove html tags
+    List<String> words = Arrays.asList(lcPage.replaceAll("[/=\",()]", " ")
         .replaceAll(" +", " ").split(" "));
     words = removeTagItems(words);
-    int iURL = words.indexOf(newUrlString.toLowerCase());
     // find any query within 5 words from the link
-    for (int i=Math.max(0, iURL-5); i<=Math.min(iURL+5, words.size()); i++) {
+    int iURL = words.indexOf(newUrlString.toLowerCase());
+    for (int i = Math.max(0, iURL - 5); i < Math.min(iURL + 6, words.size()); i++) {
       String word = words.get(i);
       if (queries.contains(word) && !fourPointSet.contains(word)) {
         fourPointSet.add(word);
@@ -187,6 +191,8 @@ public class WebCrawler {
     }
 
     // any query in the content
+    Set<String> onePointSet = new HashSet<String>(words);
+    onePointSet.retainAll(queries);
     onePointSet.removeAll(fourPointSet);
     return score + onePointSet.size();
   }
@@ -205,8 +211,19 @@ public class WebCrawler {
           knownURLs.put(url, 1);
           ScoredURL scoredURL = new ScoredURL(url, score);
           newURLs.add(scoredURL);
-          if(trace) {
+          urlsInQueue.put(url, scoredURL);
+          if (trace) {
             System.out.println("Adding to queue: " + scoredURL);
+          }
+        }
+      } else {
+        if (urlsInQueue.containsKey(url) && score > 0) {
+          ScoredURL originalURL = urlsInQueue.get(url);
+          newURLs.remove(originalURL);
+          originalURL.incrementScore(score);
+          newURLs.add(originalURL);
+          if (trace) {
+            System.out.println("Adding " + score + " to score of " + url);
           }
         }
       }
@@ -216,16 +233,17 @@ public class WebCrawler {
   }
 
   // Download contents of URL
-  public String getPage(URL url) {
+  public String getPage(ScoredURL scoredURL) {
+    URL url = scoredURL.getURL();
     try {
       URLConnection urlConnection = url.openConnection();
-      if(trace) {
-        System.out.println("Downloading " + url);
+      if (trace) {
+        System.out.println("Downloading " + scoredURL);
       }
       urlConnection.setAllowUserInteraction(false);
 
       BufferedReader urlReader = new BufferedReader(new InputStreamReader(
-          url.openStream()));
+          url.openStream(), Charset.forName("UTF-8")));
       String content = "";
       String line = "";
       while ((line = urlReader.readLine()) != null
@@ -243,26 +261,29 @@ public class WebCrawler {
   // by <a href=" ... It ends with a close angle bracket, preceded
   // by a close quote, possibly preceded by a hatch mark (marking a
   // fragment, an internal page marker)
-  public void storeAndProcessPage(URL url, String pageContent) {
+  public void storeAndProcessPage(URL url, String pageContent, int i) {
     if (!directory.isDirectory()) {
       directory.mkdir();
     }
     try {
       String urlStr = url.toString();
-      String fileName = urlStr.substring(urlStr.lastIndexOf("/")+1);
+      String fileName = urlStr.substring(urlStr.lastIndexOf("/") + 1);
       File file = new File(directory, fileName);
-      if(!file.exists()) {
+      if (!file.exists()) {
         file.createNewFile();
       }
       PrintWriter pw = new PrintWriter(file);
       pw.write(pageContent);
       pw.flush();
       pw.close();
-      if(trace) {
+      if (trace) {
         System.out.println("Received " + url);
       }
     } catch (IOException e) {
       System.out.println("ERROR: couldn't store URL ");
+    }
+    if (i == maxPages - 1) {
+      return;
     }
     String lcPage = pageContent.toLowerCase(); // Page in lower case
     int index = 0; // position in page
@@ -289,6 +310,7 @@ public class WebCrawler {
       }
       index = iEndAngle;
     }
+    System.out.println();
   }
 
   // Top-level procedure. Keep popping a url off newURLs, download
@@ -296,17 +318,18 @@ public class WebCrawler {
   public void crawl() {
     for (int i = 0; i < maxPages; i++) {
       URL url = newURLs.poll().getURL();
+      ScoredURL removed = urlsInQueue.remove(url);
       if (robotSafe(url)) {
-        String pageContent = getPage(url);
+        String pageContent = getPage(removed);
         if (pageContent.length() != 0) {
-          storeAndProcessPage(url, pageContent);
+          storeAndProcessPage(url, pageContent, i);
         }
         if (newURLs.isEmpty()) {
           break;
         }
       }
     }
-    System.out.println("Search complete.");
+    System.out.println("\nSearch complete.");
   }
 
   public static void main(String[] argv) {
